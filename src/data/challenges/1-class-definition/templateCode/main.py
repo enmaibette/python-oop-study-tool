9a668a81ce76a05277eab14b4d9b34b77bdd5111
enmaibette <enmaibette@hotmail.ch>
2026-05-22 16:52:09 +0200

    - fix exampleCode todo

--- src/data/challenges/1-class-definition/templateCode/main.py
+++ src/data/challenges/1-class-definition/templateCode/main.py
@@ -1,8 +1,7 @@
 # TODO: Define a class called Animal using CapitalizedWords notation
+
 # TODO: Use `pass` as the placeholder inside the class body
 
-
-# TODO: Create an instance of Robot and assign it to animal1
-
+# TODO: Create an instance of Animal and assign it to animal1
 
 # TODO: Print type(animal1)
