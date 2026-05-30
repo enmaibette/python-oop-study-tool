75714f7af62586a2b546df300bc5ed95656e7808
enmaibette <enmaibette@hotmail.ch>
2026-05-30 18:21:09 +0200

    add extensions

--- src/features/editor/codemirrorExtensions.ts
+++ src/features/editor/codemirrorExtensions.ts
@@ -1,10 +1,16 @@
-import { keymap, lineNumbers, EditorView } from "@codemirror/view";
-import { defaultKeymap, historyKeymap, history, indentWithTab } from "@codemirror/commands";
+import { keymap, lineNumbers, EditorView, highlightActiveLine } from "@codemirror/view";
+import {
+  defaultKeymap,
+  historyKeymap,
+  history,
+  indentWithTab,
+} from '@codemirror/commands';
 import { python } from "@codemirror/lang-python";
-import { indentUnit } from "@codemirror/language";
+import { indentUnit, bracketMatching, codeFolding, foldGutter } from '@codemirror/language';
 import { autocompletion } from "@codemirror/autocomplete";
 import { oneDark } from "@codemirror/theme-one-dark";
 import { EditorState, Extension } from "@codemirror/state";
+import { search, searchKeymap } from '@codemirror/search';
 
 const editorTheme = EditorView.theme({
   "&": { height: "100%" },
@@ -15,16 +21,33 @@ const editorTheme = EditorView.theme({
 export function buildExtensions(onChange: (value: string) => void, onRun?: () => void): Extension[] {
   return [
     lineNumbers(),
+    highlightActiveLine(),
     history(),
     python(),
     autocompletion(),
+    search(),
     oneDark,
-    keymap.of([{ key: 'Ctrl-Enter', run: () => { onRun?.(); return true; } }, indentWithTab, ...defaultKeymap, ...historyKeymap]),
+    keymap.of([
+      {
+        key: 'Ctrl-Enter',
+        run: () => {
+          onRun?.();
+          return true;
+        },
+      },
+      indentWithTab,
+      ...defaultKeymap,
+      ...searchKeymap,
+      ...historyKeymap,
+    ]),
     EditorView.updateListener.of((update) => {
       if (update.docChanged) onChange(update.state.doc.toString());
     }),
     EditorState.tabSize.of(4),
     indentUnit.of('    '),
+    bracketMatching(),
+    codeFolding(),
+    foldGutter(),
     editorTheme,
   ];
 }
